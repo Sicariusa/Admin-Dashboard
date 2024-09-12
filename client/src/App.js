3261ce6a6fe9bff4398a2f394b63d2bd343cf286
Sicariusa <128763899+Sicariusa@users.noreply.github.com>
2024-09-12 03:39:24 +0300

    Redux-setting Navbar using MUI material

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,34 @@
-
+import { Dashboard } from "@mui/icons-material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate } from "react-router-dom";
+import { themeSettings } from "theme";
+import Layout from "scenes/Layout"
+import { Dashboard } from "scenes/dashboard";
 
 function App() {
+  // we use mode from redux on theme from themeSettings that was
+  // in theme.js file you can find it in line:63
+  const mode = useSelector((state) => state.global.mode )
+  const theme = useMemo(()=> createTheme(themeSettings(mode)) , [mode] )
   return (
     <div className="app">
-      
+      <BrowserRouter>
+        <ThemeProvider theme = {theme}>
+          <CssBaseline />
+          <Routes>
+            {/* NavBar and sidebar will be shown on everypage  */}
+            {/* Or any style we want */}
+            <Route element = {<Layout />}>
+              <Route path="/" element={<Navigate to ="dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard/>} />
+              
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { configureStore } from '@reduxjs/toolkit';
+// we easily imported from state because we did edit in jsconfig
+import globalReducer from "state"
+import { Provider } from 'react-redux';
 
+const store = configureStore({
+  reducer: {
+    global: globalReducer,
+  },
+})
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store = {store}>
+      <App />
+    </Provider>
+    
   </React.StrictMode>
 );
 
